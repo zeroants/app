6f7ab52c7c10cf99c75ebcefea3c88850e3e9634
zeroants <zeroants@qq.com>
2017-10-17 05:19:54 -0500

    Add files via upload

--- data/common.inc.php
+++ data/common.inc.php
@@ -2,9 +2,9 @@
 //数据库连接信息
 $cfg_dbtype = 'mysql';
 $cfg_dbhost = 'localhost';
-$cfg_dbname = 'dedecmsv57utf8sp2';
-$cfg_dbuser = 'root';
-$cfg_dbpwd = 'xxw@75';
+$cfg_dbname = '107050b10443db';
+$cfg_dbuser = '107050b10443db';
+$cfg_dbpwd = 'OTHTdazx8493';
 $cfg_dbprefix = 'dede_';
 $cfg_db_language = 'utf8';
 
